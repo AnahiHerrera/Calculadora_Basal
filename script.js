3c3963b8f6fb21de57b22b82890898cf408be87e
AnahiHerrera <151777158+AnahiHerrera@users.noreply.github.com>
2023-11-26 17:05:21 -0300

    UPDATES

--- script.js
+++ script.js
@@ -1,14 +1,92 @@
-let peso
+let n1, n2, n3
 
-peso = 15;
+n1= 10;
+n2= 20;
+n3= 30;
 
 
-console.log(peso + 1); 
 
-let miVariable = "1";
 
-const miConstante = "10";
+if (n1 < n3 && n2 < n3) {
+console. log (true, n3)
+                        }
 
-console.log(miConstante);
+    if (n1 < n3 && n3 < n2)  {
+    console. log (true)
+                         }
 
-// NUMBER, STRING
+    else {
+    console.log (false)
+}
+ if (n1 < n3 || n2 < n3) {
+    console.log (true)
+
+ }
+    else {
+    console. log (false)
+        }
+
+ function mensaje ()    {
+    console. log ("hola hola")
+                        }
+ mensaje ();
+
+    function mayor (n1, n2, n3) 
+    {
+        if (n1 < n3 && n3 < n2) {
+            console. log (30)
+                                }
+        
+        else {
+            console.log (10)
+             }
+    }
+    mayor (10, 20, 30)
+
+    function suma (n1, n2) {
+    console . log ( n1 + n2) 
+    }
+    suma( 10, 20)
+
+mayor (10, 20, 30)
+
+/* function suma (n1, n2) {
+let resultado =( n1 + n2) 
+return resultado
+}
+suma( 10, 20)*/ 
+
+let  peso = 5
+    function holliday (peso) {
+        if (peso <= 10)    { resultado = peso * 100 
+        return (resultado) }
+
+                else if ( peso => 10 && peso >= 20) 
+                        {resultado = ((peso - 10) * 50) + 1000
+        return(resultado)                                 
+                    } 
+
+                else if (peso > 20) 
+                {resultado = ((peso - 10) * 20) + 1500
+        return (resultado)
+                }
+
+                        }
+
+console.log (holliday(peso))
+console.log (`resultado" ${holliday(peso)}, el mantenimiento es ${holliday(peso)/24}`)
+
+peso = 35
+function SC (peso) {
+
+{resultado = ((peso * 4) + 7) / (peso + 90)}
+return (resultado * 1500)
+
+}
+
+
+
+console.log (SC (peso))
+
+    
+
