1703d2c6d1d0b898122b247827322b04f4144684
AnahiHerrera <151777158+AnahiHerrera@users.noreply.github.com>
2023-11-28 23:50:00 -0300

    Se agrego Js.

--- script.js
+++ script.js
@@ -1,92 +1,60 @@
-let n1, n2, n3
+const input = document.getElementById("peso");
+const boton = document.getElementById("calcular");
+const error = document.getElementById("error");
+const elementosResultado = document.querySelectorAll('.resultado')
 
-n1= 10;
-n2= 20;
-n3= 30;
+function eventoDelBoton() {
+    let peso = input.value.trim();
+    let resultado;
 
-
-
-
-if (n1 < n3 && n2 < n3) {
-console. log (true, n3)
-                        }
-
-    if (n1 < n3 && n3 < n2)  {
-    console. log (true)
-                         }
-
-    else {
-    console.log (false)
-}
- if (n1 < n3 || n2 < n3) {
-    console.log (true)
-
- }
-    else {
-    console. log (false)
-        }
-
- function mensaje ()    {
-    console. log ("hola hola")
-                        }
- mensaje ();
-
-    function mayor (n1, n2, n3) 
-    {
-        if (n1 < n3 && n3 < n2) {
-            console. log (30)
-                                }
-        
-        else {
-            console.log (10)
-             }
+    if (peso.length <= 0) {
+        error.style.display = 'block';
+        elementosResultado.forEach(function(elemento) {
+            elemento.style.display = 'none';
+        });
+        return;
     }
-    mayor (10, 20, 30)
 
-    function suma (n1, n2) {
-    console . log ( n1 + n2) 
+    if (peso <= 30) {
+        resultado = holliday(peso);
+    } else {
+        resultado = SC(peso);
     }
-    suma( 10, 20)
 
-mayor (10, 20, 30)
 
-/* function suma (n1, n2) {
-let resultado =( n1 + n2) 
-return resultado
+    error.style.display = 'none';
+    elementosResultado.forEach(function(elemento) {
+        elemento.style.display = 'block';
+    });
+
+    elementosResultado[0].innerHTML = "Volumen: " + resultado + " cc"; 
+    elementosResultado[1].innerHTML = "Mantenimiento: "+ Math.ceil(resultado/24) + " cc/hr"; 
+    elementosResultado[2].innerHTML = "m+m/2: " + Math.ceil(resultado/24*1.5) + " cc/hr"; 
 }
-suma( 10, 20)*/ 
 
-let  peso = 5
-    function holliday (peso) {
-        if (peso <= 10)    { resultado = peso * 100 
-        return (resultado) }
 
-                else if ( peso => 10 && peso >= 20) 
-                        {resultado = ((peso - 10) * 50) + 1000
+
+function holliday (peso) {
+    if (peso <= 10){ 
+        resultado = peso * 100 
+        return (resultado) 
+    }
+    else if ( peso <= 20){
+        resultado = ((peso - 10) * 50) + 1000
         return(resultado)                                 
-                    } 
-
-                else if (peso > 20) 
-                {resultado = ((peso - 10) * 20) + 1500
+    } 
+    else if (peso > 20){
+        resultado = ((peso - 20) * 20) + 1500
         return (resultado)
-                }
-
-                        }
-
-console.log (holliday(peso))
-console.log (`resultado" ${holliday(peso)}, el mantenimiento es ${holliday(peso)/24}`)
-
-peso = 35
-function SC (peso) {
-
-{resultado = ((peso * 4) + 7) / (peso + 90)}
-return (resultado * 1500)
-
+    }
 }
 
 
+function SC(peso) {
+    resultado = ((peso * 4) + 7) / (+peso + 90)
+    return (resultado * 1500)
+}
 
-console.log (SC (peso))
+boton.addEventListener("click", eventoDelBoton)
 
-    
 
